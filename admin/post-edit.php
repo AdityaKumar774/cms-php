6483fbd1b5a317f3514f4c6468d4d85e71987fd6
Aditya Kumar <aditya.kumar774@gmail.com>
2017-09-17 02:02:48 +0530

    basic setup

---
+++ admin/post-edit.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <title>Insert Post</title>
+</head>
+<body>
+<p>
+    <input type="text" name="post_title" />
+</p>
+<p>
+    <textarea name="post_content"></textarea>
+</p>
+<p>
+    <label for="post_category">Cat 1</label>
+    <input type="checkbox" name="post_category" value="cat1" />
+    <br />
+    <label for="post_category">Cat 2</label>
+    <input type="checkbox" name="post_category" value="cat2" />
+</p>
+<p>
+    <input type="submit" value="Submit" />
+</p>
+</body>
+</html>

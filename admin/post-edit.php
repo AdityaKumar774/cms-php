ef02acd2e542876ee13a7cbd908cdf88ca79901e
Aditya Kumar <aditya.kumar774@gmail.com>
2017-09-17 02:45:12 +0530

    edit-post

--- admin/post-edit.php
+++ admin/post-edit.php
@@ -1,24 +1,26 @@
-<!doctype html>
+<pre><?php print_r($_POST); ?></pre>
 <html>
 <head>
     <title>Insert Post</title>
 </head>
 <body>
-<p>
-    <input type="text" name="post_title" />
-</p>
-<p>
-    <textarea name="post_content"></textarea>
-</p>
-<p>
-    <label for="post_category">Cat 1</label>
-    <input type="checkbox" name="post_category" value="cat1" />
-    <br />
-    <label for="post_category">Cat 2</label>
-    <input type="checkbox" name="post_category" value="cat2" />
-</p>
-<p>
-    <input type="submit" value="Submit" />
-</p>
+    <form method="post">
+        <p>
+            <input type="text" name="post_title" />
+        </p>
+        <p>
+            <textarea name="post_content"></textarea>
+        </p>
+        <p>
+            <label for="post_category">Cat 1</label>
+            <input type="checkbox" name="post_category" value="cat1" />
+            <br />
+            <label for="post_category">Cat 2</label>
+            <input type="checkbox" name="post_category" value="cat2" />
+        </p>
+        <p>
+            <input type="submit" value="Submit" />
+        </p>
+    </form>
 </body>
 </html>
